ce4906cf6864119be24cecf9dbe48180c97ae47f
Matteo <yuky2020@gmail.com>
2020-12-30 22:17:29 +0100

    Some test with audio recording

---
+++ cmake-build-debug/CMakeFiles/CSaP_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSaP_Project.dir/audio/paex_record.c.o"
+  "CSaP_Project"
+  "CSaP_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CSaP_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
