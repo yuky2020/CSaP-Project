e27ffa132615590ef94b64cc86c3afa727e721ee
yuky2020 <yuky2020@gmail.com>
2021-07-01 21:23:15 +0200

    Added some dependency check, a bit professionality

--- install.sh
+++ install.sh
@@ -5,10 +5,18 @@ if [[ `cat /etc/os-release | grep VERSION_CODENAME` != 'VERSION_CODENAME=hirsute
     echo "Must be on a Debian derivate to run this script"
     exit
 fi
+#more professional than before for sure 
+# this part is for check if make and gcc are installed since vanilla version of debian could not include this build essental package
+allReqPackage=("gcc" "make" "libjack-dev" "libsndfile-dev" "libasound2-dev")
+for i in ${!allReqPackage[@]}; do
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ${allReqPackage[$i]}|grep "install ok installed")
+echo Checking for ${allReqPackage[$i]}: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No ${allReqPackage[$i]}. Setting up ${allReqPackage[$i]}."
+  sudo apt-get --yes install ${allReqPackage[$i]} 
+fi
+done
 
-sudo apt install libjack-dev
-sudo apt-get install libsndfile-dev
-sudo apt-get install libasound2-dev
 cd Mds
 make 
 cd .. 
