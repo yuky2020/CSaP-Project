da1e35b623c3fcbc223fc73ee01f1cf23c2abd5f
yuky2020 <yuky2020@gmail.com>
2021-07-01 21:06:01 +0200

    As prof suggest added systems control on scripts,

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#system type check
+
+if [[ `cat /etc/os-release | grep VERSION_CODENAME` != 'VERSION_CODENAME=hirsute' && `cat /etc/os-release | grep VERSION_CODENAME` != 'VERSION_CODENAME=buster'  ]]; then
+    echo "Must be on a Debian derivate to run this script"
+    exit
+fi
+
 sudo apt install libjack-dev
 sudo apt-get install libsndfile-dev
 sudo apt-get install libasound2-dev
